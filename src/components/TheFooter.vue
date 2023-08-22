10917a833c2f98f175956f10e4fe6ae1ac05ff2e
masterkudi <cudini.andrea@gmail.com>
2023-08-23 00:10:42 +0200

    procedo intanto con il footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,13 +7,23 @@ export default {
 
 <template>
     <footer>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta obcaecati voluptate dolor accusantium. Laboriosam ex ea assumenda cum optio dolores unde, error sunt deleniti corporis sapiente commodi voluptatibus ipsum at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque porro ea dolores nemo optio magnam rerum consequatur iusto illo quisquam tempora, assumenda ad libero nulla, odio officiis ducimus eveniet. Quaerat.
+        <img src="../assets/img/dc-logo-bg.png" alt="">
+    
     </footer>
 </template>
   
 <style scoped>
     footer {
         background-image: url(../assets/img/footer-bg.jpg);
+        color: #fff;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    footer img {
+        position: relative;
+        width: 20rem;
     }
 </style>
   
