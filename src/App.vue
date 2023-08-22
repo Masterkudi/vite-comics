3cb868173d8a0f15152249ab7cbe0dab614c036e
masterkudi <cudini.andrea@gmail.com>
2023-08-22 15:31:23 +0200

    inizializzazione

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from '@/components/TheHeader'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader,
+    Main,
+    TheFooter
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "TheHeader",
+    data() {
+
+    },
+    methods: {
+
+    }
+}
+
+</script>
