d6f7edc7622726e0a23339ae057096885a86b1b7
masterkudi <cudini.andrea@gmail.com>
2023-08-23 17:10:19 +0200

    completamento footer secondario

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,24 +7,76 @@ export default {
 
 <template>
     <footer>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque porro ea dolores nemo optio magnam rerum consequatur iusto illo quisquam tempora, assumenda ad libero nulla, odio officiis ducimus eveniet. Quaerat.
-        <img src="../assets/img/dc-logo-bg.png" alt="">
-    
+        <div class="menu-bar">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque porro ea dolores nemo optio magnam rerum
+            consequatur iusto illo quisquam tempora, assumenda ad libero nulla, odio officiis ducimus eveniet. Quaerat.
+            <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+
+        <div class="social-bar">
+            <button>
+                SIGN-UP NOW!
+            </button>
+            <div class="social-icons">
+                <h4>FOLLOW US</h4>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+            </div>
+        </div>
     </footer>
 </template>
   
 <style scoped>
-    footer {
-        background-image: url(../assets/img/footer-bg.jpg);
-        color: #fff;
-        display: flex;
-        position: relative;
-        justify-content: flex-end;
-        max-height: 500px;
+.menu-bar {
+    background-image: url(../assets/img/footer-bg.jpg);
+    color: #fff;
+    display: flex;
+    position: relative;
+    justify-content: flex-end;
+    max-height: 500px;
+}
 
-    footer img {
-        overflow: hidden;
-    }
-    }
+.menu-bar img {
+    overflow: hidden;
+}
+
+.social-bar {
+    background-color: #303030;
+    padding: 3rem;
+    display: flex;
+    justify-content: space-between;
+
+}
+
+.social-bar h4 {
+    display: flex;
+    align-items: center;
+    color: #0088FF;
+}
+
+.social-bar button {
+    display: flex;
+    align-items: center;
+    background-color: #303030;
+    color: #fff;
+    padding: .7rem;
+    border-color: #0088FF;
+}
+
+.social-icons {
+    display: flex;
+    justify-content: space-between;
+}
+.social-icons h4 {
+    padding: .5rem;
+    margin-bottom: 0;
+}
+.social-icons img {
+    padding: .5rem;
+}
+
 </style>
   
